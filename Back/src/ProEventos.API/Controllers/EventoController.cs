6ab60981c3a4cd927188bd8a132b6f602fdb4397
Pedro Braga <pedro.braga@prevenirassistencial.com.br>
2023-03-05 09:13:39 -0300

    EF Core

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,43 +13,24 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
+        public DataContext Context { get; }
 
-        public IEnumerable<Evento> _evento = new Evento[]{
-                new Evento() {
-                EventoId = 1,
-                Tema = "Angular 11 e .NET 5",
-                Local = "BH",
-                Lote = "1º Lote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                ImagemURL = "foto.png"
-                },
-                new Evento() {
-                EventoId = 2,
-                Tema = "Angular 11 e .NET 5",
-                Local = "SP",
-                Lote = "2º Lote",
-                QtdPessoas = 350,
-                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
-                ImagemURL = "foto.png"
-                }
-            };
-
-        public EventoController()
+        public EventoController(DataContext context)
         {
+            this.Context = context;
           
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetByid(int id)
+        public Evento GetByid(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return Context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return Context.Eventos;
         }
 
         [HttpPost]
